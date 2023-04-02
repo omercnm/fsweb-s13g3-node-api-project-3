978cca330f3b880e05e379accb87aaed59b6e96f
ömer <omercnm23@gmail.com>
2023-04-02 18:39:54 +0300

    S13G3 tamamlandı

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-
+const express = require("express");
+const userRouter = require("./users/users-router");
+const { logger } = require("./middleware/middleware");
 const server = express();
 
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
-
+server.use(express.json());
 // global ara yazılımlar ve kullanıcı routelarının buraya bağlanması gerekir
+server.use(logger);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
 });
 
